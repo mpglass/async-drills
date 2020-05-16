cbdec7d345e4b58923d6ca84d339234cfa4ccb64
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-05-16 15:21:12 -0500

    TTT finished

--- app.js
+++ app.js
@@ -1,33 +1,58 @@
-function sayMessage(message) {
-    console.log(message)
-};
-sayMessage('Hello World')
-setTimeout(function () { sayMessage('2 seconds'); }, 2000);
+// function sayMessage(message) {
+//     console.log(message)
+// };
+// sayMessage('Hello World')
+// setTimeout(function () { sayMessage('2 seconds'); }, 2000);
 
-function getWords(word) {
-    console.log(word);
-};
+// function getWords(word) {
+//     console.log(word);
+// };
 
-// setTimeout(function(){ alert("Hello"); }, 3000);
-function waitForIt() {
-    getWords('Wait');
-    setTimeout(function () { getWords('for'); }, 3000);
-    setTimeout(function () { getWords('the'); }, 5000);
-    setTimeout(function () { getWords('BOOM!'); }, 6000);
+// function waitForIt() {
+//     getWords('Wait');
+//     setTimeout(function () { getWords('for'); }, 3000);
+//     setTimeout(function () { getWords('the'); }, 5000);
+//     setTimeout(function () { getWords('BOOM!'); }, 6000);
+// }
+
+// waitForIt();
+
+
+// const countdown = (num, callback) => {
+    
+//     setTimeout(() => {
+
+
+//         if (num > 0) {
+//             console.log(num);
+//             countdown(num - 1, callback);
+//         }
+//         else {
+//             callback();
+//         }
+//     }, 1000);
+// }
+
+// const done = () => {
+//     console.log('Job\'s done!')
+// };
+
+// countdown(5, done);
+
+var lunchTime = true;
+const orderMeSomeFood = () => {
+    return new Promise((resolve, reject) => {
+if lunchTime === true {
+    resoleve (lunch, drink);
+} else {
+    reject(error);
+    )
 }
-
-waitForIt();
-
-function done() {
-    console.log('Job\'s done!');
+    });
 };
 
-function countdown(num) {
-    if (num > 0) {
-        console.log(num);
-        countdown(num - 1);
-    }
-    else {
-        console.log(done());
-    }
-};
+// const orderMeSomeFood = () => {
+//     return new Promise((resolve, reject) => {
+      
+//     });
+//   };
